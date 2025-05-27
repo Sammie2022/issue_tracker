3f4991e34bdef081c36844305ccdb115adac046e
Sammie2022 <samuelnganganganga26@gmail.com>
2025-05-27 12:28:04 +0300

    Add project model, controller, views, and full CRUD functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,64 @@
 class ProjectsController < ApplicationController
-end
+    before_action :set_project, only: %i[show edit update destroy]
+ 
+    def index
+        @projects = Project.all
+    end
+
+    def show
+        @issues = @project.issues
+    end
+
+    def new
+      @project = Project.new
+    end
+    
+    def create
+      @project = Project.new(project_params)
+      if @project.save
+        redirect_to @project, notice: 'Project was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    def edit; end
+
+    def update
+        if @project.update(project_params)
+            redirect_to @project, notice: 'Project was successfully updated.'
+        else
+            render :edit
+        end
+        end
+    
+        def destroy
+        @project.destroy
+        redirect_to projects_url, notice: 'Project was successfully destroyed.'
+        end
+    
+        private
+    
+        def set_project
+        @project = Project.find(params[:id])
+        end
+    
+        def project_params
+        params.require(:project).permit(:name, :description, :start_date, :end_date)
+        end
+    end
+
+    def destroy
+        @project.destroy
+        redirect_to projects_url, notice: 'Project was successfully destroyed.'
+    end
+
+    private
+
+    def set_project
+        @project = Project.find(params[:id])
+    end
+
+    def project_params
+        params.require(:project).permit(:name, :description, :start_date, :end_date)
+    end
