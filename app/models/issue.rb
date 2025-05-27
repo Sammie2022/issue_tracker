5c8903dd52cccf94a1ffd6d3a4a16cd9251253ac
Sammie2022 <samuelnganganganga26@gmail.com>
2025-05-27 11:42:53 +0300

    defined ralationships and rules in models

---
+++ app/models/issue.rb
@@ -0,0 +1,9 @@
+class Issue < ApplicationRecord
+  belongs_to :project
+
+  STATUSES = ['New', 'In Progress', 'Closed']
+
+  validates :title, presence: true
+  validates :status, inclusion: { in: STATUSES }
+  validates :priority, inclusion: {in 1..5}
+end
